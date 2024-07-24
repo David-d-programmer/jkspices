f5359d7ba29f3a6f4a374a565bf2fc2182976ae4
David Okon <davidokon769@gmail.com>
2024-07-24 16:10:48 +0000

    first

--- Jkspices/settings.py
+++ Jkspices/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_9dh1*5%1-hji-pduc0aru7woircn)v!2mt(z^&7^r#f2!a+%j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-daviddprogramme-projno4-v8gp9gebo8y.ws-eu115.gitpod.io','.herokuapp.com',
