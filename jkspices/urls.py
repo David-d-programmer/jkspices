affad7e762666b745bc2573d59c020db31c56c34
David Okon <davidokon769@gmail.com>
2024-07-26 09:05:03 +0000

    updating my files

--- jkspices/urls.py
+++ jkspices/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Jkspices project.
+URL configuration for jkspices project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
--- jkspices/wsgi.py
+++ jkspices/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Jkspices project.
+WSGI config for jkspices project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Jkspices.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jkspices.settings')
 
 application = get_wsgi_application()
